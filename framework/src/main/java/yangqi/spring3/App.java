b2f10f05dfd5ef307977a2abb7285cc7c1476519
llohellohe <yangqichris@gmail.com>
2013-03-24 21:28:32 +0800

    first commit

---
+++ framework/src/main/java/yangqi/spring3/App.java
@@ -0,0 +1,13 @@
+package yangqi.spring3;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
